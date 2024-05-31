2bd248236d34af39036b3e8b61ad42f027d76431
M2rgus <karlmarkus.saareli@tptlive.ee>
2024-05-31 11:32:01 +0300

    Lisatud teste

--- app.test.js
+++ app.test.js
@@ -8,24 +8,44 @@ const app = createApp(validateUsername, validatePassword)
 describe('given correct username and password', () => {
     test('return status 200', async () => {
         const response = await request(app).post('/users').send({
+
             username: 'Username',
             password: 'Password123'
+
         })
         expect(response.statusCode).toBe(200)
     })
 
     test('returns userId', async () => {
         const response = await request(app).post('/users').send({
+
             username: 'Username',
             password: 'Password123'
+
         })
         expect(response.body.userId).toBeDefined();
     })
 
-    // test response content type?
-    // test response message
-    // test response user id value
-    // ...
+    test('returns correct message in response body', async () => {
+        const response = await request(app).post('/users').send({
+
+            username: 'Username',
+            password: 'Password123'
+
+        })
+        expect(response.body.message).toBe("Valid User");
+    })
+
+    test('returns correct user id value in body', async () => {
+        const response = await request(app).post('/users').send({
+
+            username: 'Username',
+            password: 'Password123'
+
+        })
+        expect(response.body.userId).toBe("1");
+    })
+
 })
 
 describe('given incorrect or missing username and password', () => {
@@ -37,9 +57,64 @@ describe('given incorrect or missing username and password', () => {
         expect(response.statusCode).toBe(400)
     })
 
-    // test response message
-    // test that response does NOT have userId
-    // test incorrect username or password according to requirements
-    // test missing username or password
-    // ...
+    test('testing missing username', async () => {
+        const response = await request(app).post('/users').send({
+            username: '',
+            password: 'Password123'
+        })
+        expect(response.statusCode).toBe(400)
+    })
+
+    test('testing missing password', async () => {
+        const response = await request(app).post('/users').send({
+            username: 'Username',
+            password: ''
+        })
+        expect(response.statusCode).toBe(400)
+    })
+
+    test('testing both username and password are null', async () => {
+        const response = await request(app).post('/users').send({
+            username: '',
+            password: ''
+        })
+        expect(response.statusCode).toBe(400)
+    })
+
+    test('return no userId', async () => {
+        const response = await request(app).post('/users').send({
+            username: 'user',
+            password: 'password'
+        })
+        expect(response.body.userId).toBeUndefined()
+    })
+
+    test('test response body is correct', async () => {
+        const response = await request(app).post('/users').send({
+            username: 'user',
+            password: 'password'
+        })
+        expect(response.body.error).toBe("Invalid User")
+    })
+
+    test('test username requirements', async () => {
+        const response = await request(app).post('/users').send({
+
+            username: 'User',
+            password: 'Password123'
+
+        })
+        expect(response.body.error).toBe("Invalid User")
+    })
+
+    test('test password requirements', async () => {
+        const response = await request(app).post('/users').send({
+
+            username: 'Username',
+            password: 'password'
+
+        })
+        expect(response.body.error).toBe("Invalid User")
+    })
+    
 })
